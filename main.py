08bca0c7f1861453d7bbace9aed0f573a020852d
ahfasxp <ahfas.fazria@gmail.com>
2025-05-15 10:19:09 +0700

    feat: Add TTS server and client executors with updated configuration for English language support

---
+++ main.py
@@ -0,0 +1,4 @@
+from paddlespeech.cli.tts.infer import TTSExecutor
+
+tts = TTSExecutor()
+tts(am="fastspeech2_ljspeech",voc="hifigan_ljspeech",lang="en", text="Life was like a box of chocolates, you never know what you're gonna get.", output="output.wav")
